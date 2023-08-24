e0c7cdedbbcd04949a6e994e3614831df37c30e2
Jenniferbailey032 <Jenniferbailey032@gmail.com>
2023-08-24 16:31:43 -0400

    connect page_event gallery

--- js/scripts.js
+++ js/scripts.js
@@ -19,3 +19,22 @@ mainMenu.addEventListener("click", () => {
         mainMenu.classList.remove("show-menu");
         canSeeMenu = false;
 })
+
+const wrapper = document.querySelector(".wrapper"),
+    carousel = document.querySelector(".carousel "),
+    images = document.querySelectorALL(".img"),
+    buttons = document.querySelectorALL(".button");
+
+let imageIndex =1,
+intervalid;
+
+const autoSlide = () => {
+    intervalid = setInterval(() => slideImage(++imageIndex), 2000)
+};
+
+autoSlide()
+
+const slideImage = () => {
+    console.style.transform = 'translate(-${imageIndex * 100}%)'
+};
+
