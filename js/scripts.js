61c7b7fc2b671c5649ee8fbdf29c6136f5af82d1
Jenniferbailey032 <Jenniferbailey032@gmail.com>
2023-08-25 13:54:14 -0400

    medical changes

--- js/scripts.js
+++ js/scripts.js
@@ -20,21 +20,31 @@ mainMenu.addEventListener("click", () => {
         canSeeMenu = false;
 })
 
-const wrapper = document.querySelector(".wrapper"),
-    carousel = document.querySelector(".carousel "),
-    images = document.querySelectorALL(".img"),
-    buttons = document.querySelectorALL(".button");
 
-let imageIndex =1,
-intervalid;
+let slideIndex = 1;
+showSlides(slideIndex);
 
-const autoSlide = () => {
-    intervalid = setInterval(() => slideImage(++imageIndex), 2000)
-};
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
 
-autoSlide()
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
 
-const slideImage = () => {
-    console.style.transform = 'translate(-${imageIndex * 100}%)'
-};
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
 
