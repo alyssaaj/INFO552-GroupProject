8a1c962aebeed3da96209874258f589ca69afa7d
Alyssa Jordan <alj81@pitt.edu>
2023-08-29 15:16:17 -0400

    dark mode and small size logo added.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// Nav Burger Menu
 let burgerBtn = document.querySelector("#burger-btn");
 let mainMenu = document.querySelector("#main-menu");
 
@@ -20,8 +21,22 @@ mainMenu.addEventListener("click", () => {
         canSeeMenu = false;
 })
 
+// Dark Mode
+let toggleBtn = document.querySelector(".dark-mode-toggle");
 
-let slideIndex = 1;
+let sun = document.querySelector(".fa-sun");
+let moon = document.querySelector(".fa-moon");
+
+toggleBtn.addEventListener("click", function () {
+  document.querySelector('html').toggleAttribute('data-dark-mode')
+
+
+  sun.classList.toggle('hide-sun')
+  moon.classList.toggle('hide-moon')
+});
+
+// Slide Show
+/*let slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
@@ -36,15 +51,14 @@ function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
+  if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+    slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";  
+  slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
-}
-
+}*/
